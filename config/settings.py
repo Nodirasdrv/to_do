e011684f2bb62fa3ca0d0cda6306384d39bea1fb
Nodira <75232751+Nodirasdrv@users.noreply.github.com>
2022-04-14 09:43:25 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -132,5 +132,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nodirasdrv@gmail.com'
-EMAIL_HOST_PASSWORD = 'fismpkynjquzhczg'
+EMAIL_HOST_PASSWORD = ''
 
